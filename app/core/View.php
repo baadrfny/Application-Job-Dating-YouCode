2f6d38bd7e7e8661dd1856c01c4aad362e133277
Mohammed Darkaoui <darkaouimeed12@gmail.com>
2026-01-21 11:31:58 +0100

    finishing the login & register part

--- app/core/View.php
+++ app/core/View.php
@@ -4,7 +4,28 @@ namespace core;
 
 class View
 {
-    private static function make(string $view, array $data = []): string
+    private static function interpolate(string $template, array $data = [], array $rawKeys = []): string
+    {
+        foreach ($data as $key => $value) {
+            if (is_array($value) || is_object($value)) {
+                continue;
+            }
+
+            $replace = in_array($key, $rawKeys, true)
+                ? (string) $value
+                : Security::escape((string) $value);
+
+            $template = str_replace(
+                ['{{ ' . $key . ' }}', '{{' . $key . '}}'],
+                $replace,
+                $template
+            );
+        }
+
+        return $template;
+    }
+
+    private static function make(string $view, array $data = [], array $rawKeys = []): string
     {
         $viewPath = str_replace('.', DIRECTORY_SEPARATOR, $view);
         $file = dirname(__DIR__) . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . $viewPath . ".twig";
@@ -15,7 +36,8 @@ class View
         }
 
         // For now, return simple content - Twig integration can be added later
-        return file_get_contents($file);
+        $template = file_get_contents($file);
+        return self::interpolate($template, $data, $rawKeys);
     }
 
     public static function render(string $view, array $data = [], string $layout = 'layouts.main'): string
@@ -24,6 +46,6 @@ class View
 
         return self::make($layout, array_merge($data, [
             'content' => $content,
-        ]));
+        ]), ['content']);
     }
 }
