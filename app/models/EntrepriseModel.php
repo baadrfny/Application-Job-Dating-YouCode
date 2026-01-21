11521b948555cf928ffb0c7cfe79174ef1b9fe2a
KacimiLahcen <kacimi.lahcen88@gmail.com>
2026-01-21 11:43:51 +0100

    my process rn

--- app/models/EntrepriseModel.php
+++ app/models/EntrepriseModel.php
@@ -7,14 +7,6 @@ use core\Model;
 class EntrepriseModel extends Model {
     protected string $table = 'entreprises';
 
-    // التحقق من وجود إعلانات مرتبطة قبل الحذف (Delete Policy)
-    public function canDelete(int $id): bool {
-        $sql = "SELECT COUNT(*) FROM annonces WHERE entreprise_id = :id";
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute(['id' => $id]);
-        return $stmt->fetchColumn() == 0; // if not exist ; returns true 
-    }
-
     // (Unique)
     public function isEmailExists(string $email): bool {
         return $this->count("email = :email", ['email' => $email]) > 0;
